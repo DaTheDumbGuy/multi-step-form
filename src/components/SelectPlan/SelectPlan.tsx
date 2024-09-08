bf682d1407e6724bc6570e21ba42c3f9c4c8bbba
Daryl Bacurin <darylbacurin14@gmail.com>
2024-09-09 07:00:36 +0800

    Plan style update

--- src/components/SelectPlan/SelectPlan.tsx
+++ src/components/SelectPlan/SelectPlan.tsx
@@ -2,19 +2,38 @@ import ArcadeIcon from "../../assets/images/icon-arcade.svg";
 import AdvancedIcon from "../../assets/images/icon-advanced.svg";
 import ProIcon from "../../assets/images/icon-pro.svg";
 import { FormsProps } from "../../types/types";
-import './test.scss';
 import { useState } from "react";
 import { handleSubmit } from "../../utils/handleSubmit";
 import FormHeader from "../Ui/FormHeader/FormHeader";
 import SubmitButton from "../Ui/SubmitButton/SubmitButton";
+import styles from './selectPlan.module.scss';
 
 export default function SelectPlan({nextStep, updateFormValues}: FormsProps) {
-
   const [isYearly, setIsYearly] = useState(false);
 
   const handleToggle = () => {
     setIsYearly(!isYearly);
   };
+  const handlePlan = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.currentTarget;
+    const radioInput = target.previousElementSibling as HTMLInputElement;
+  
+    if (!radioInput.checked) {
+      // Uncheck all radio inputs first
+      const allPlans = document.querySelectorAll(`input[name="plan"]`);
+      allPlans.forEach((input) => {
+        const planContent = input.nextElementSibling as HTMLElement;
+        if (planContent.classList.contains(styles['active'])) {
+          planContent.classList.remove(styles['active']);
+        }
+      });
+  
+      // Set the clicked plan as checked and add the active class
+      radioInput.checked = true;
+      target.classList.add(styles['active']);
+    }
+  };
+  
   const submitHandler = handleSubmit({
     updateFormValues,
     onSuccess: () => {
@@ -22,63 +41,75 @@ export default function SelectPlan({nextStep, updateFormValues}: FormsProps) {
         nextStep();
     }
 });
+console.log(styles);
   return (
     <>
       <FormHeader
         title="Select your plan"
         description="You have the option of monthly or yearly billing."
       />
-      <form className="pricing-plan" onSubmit={submitHandler}>
-        <label className="plan">
-          <input type="radio" name="plan" value="arcade" hidden />
-          <div className="plan__content">
+      <form className={styles['b-pricePlan']} onSubmit={submitHandler}>
+        <label className={styles['b-pricePlan__plan']}>
+          <input type="radio" name="plan" value="arcade" hidden checked/>
+          <div className={`${styles['b-pricePlan__plan__content']} ${styles.active}`} onClick={handlePlan}>
             <div className="plan__icon">
               <img src={ArcadeIcon} alt="" />
             </div>
-            <div className="plan__title">Arcade</div>
-            <div className="plan__price">$9/mo</div>
+            <div className={styles['b-pricePlan__plan__content__description']}>
+              <h2 className="plan__title">Arcade</h2>
+              <p className="plan__price">$9/mo</p>
+            </div>
           </div>
         </label>
-        <label className="plan">
+
+        <label className={styles['b-pricePlan__plan']}>
           <input type="radio" name="plan" value="advanced" hidden />
-          <div className="plan__content">
+          <div className={styles['b-pricePlan__plan__content']}  onClick={handlePlan}>
             <div className="plan__icon">
               <img src={AdvancedIcon} alt="" />
             </div>
-            <div className="plan__title">Advanced</div>
-            <div className="plan__price">$12/mo</div>
+            <div className={styles['b-pricePlan__plan__content__description']}>
+              <h2 className="plan__title">Advanced</h2>
+              <p className="plan__price">$12/mo</p>
+            </div>
           </div>
         </label>
-        <label className="plan">
+
+        <label className={styles['b-pricePlan__plan']}>
           <input type="radio" name="plan" value="pro" hidden />
-          <div className="plan__content">
+          <div className={styles['b-pricePlan__plan__content']} onClick={handlePlan}>
             <div className="plan__icon">
               <img src={ProIcon} alt="" />
             </div>
-            <div className="plan__title">Pro</div>
-            <div className="plan__price">$15/mo</div>
+            <div className={styles['b-pricePlan__plan__content__description']}>
+              <h2 className="plan__title">Pro</h2>
+              <p className="plan__price">$15/mo</p>
+            </div>
           </div>
         </label>
-        <div className="toggle">
-        <span>Monthly</span>
-        <button 
-          type="button" 
-          className="testButton" 
-          onClick={handleToggle}
-          aria-label={`Switch to ${isYearly ? "monthly" : "yearly"} plan`}
-        >
-          <div className={`circle ${isYearly ? "yearly" : "monthly"}`}></div>
-        </button>
-        <input 
-          type="hidden" 
-          name="subscription" 
-          value={isYearly ? "yearly" : "monthly"} 
-          readOnly 
-        />
-        <span>Yearly</span>
-      </div>
-        <SubmitButton/>
+
+        <div className={styles['b-pricePlan__subscription']}>
+          <span>Monthly</span>
+          <button 
+            type="button" 
+            className={styles['b-pricePlan__subscription__button']}
+            onClick={handleToggle}
+            aria-label={`Switch to ${isYearly ? "monthly" : "yearly"} plan`}
+          >
+            <div className={`${styles['b-pricePlan__subscription__button__circle']} ${isYearly ? styles.yearly: styles.monthly}`}></div>
+          </button>
+          <input 
+            type="hidden" 
+            name="subscription" 
+            value={isYearly ? "yearly" : "monthly"} 
+            readOnly 
+          />
+          <span>Yearly</span>
+        </div>
+
+        <SubmitButton />
       </form>
+
     </>
   );
 }
